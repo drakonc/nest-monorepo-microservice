980f746a05de1d49021ff88024a4baa7e834fa59
Jose Castro <drakonc@gmail.com>
2022-07-27 19:09:19 -0500

    Conexion A Base de datos

--- apps/orders/src/app.module.ts
+++ apps/orders/src/app.module.ts
@@ -1,10 +1,26 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { OrdersRepository } from './repositories/orders.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required()
+      }),
+      envFilePath: './apps/orders/.env'
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([{name: Order.name, schema: OrderSchema}])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, OrdersRepository],
 })
 export class AppModule {}
